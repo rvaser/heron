dc1a9473ab4a467570aa82e206ec67ada3b37568
rvaser <robert.vaser@gmail.com>
2021-08-10 18:10:20 +0800

    Add options

--- src/main.cpp
+++ src/main.cpp
@@ -4,12 +4,14 @@
 
 #include <cstdint>
 #include <iostream>
+#include <unordered_map>
 
 #include "bioparser/fasta_parser.hpp"
 #include "bioparser/fastq_parser.hpp"
 #include "biosoup/nucleic_acid.hpp"
 #include "biosoup/timer.hpp"
 #include "edlib.h"  // NOLINT
+#include "ksw2.h"  // NOLINT
 #include "ram/minimizer_engine.hpp"
 #include "thread_pool/thread_pool.hpp"
 
@@ -18,6 +20,8 @@ std::atomic<std::uint32_t> biosoup::NucleicAcid::num_objects{0};
 namespace {
 
 static struct option options[] = {
+  {"frequencies", no_argument, nullptr, 'f'},
+  {"ksw2", no_argument, nullptr, 'k'},
   {"threads", required_argument, nullptr, 't'},
   {"version", no_argument, nullptr, 'v'},
   {"help", no_argument, nullptr, 'h'},
@@ -68,6 +72,12 @@ void Help() {
       "    input file in FASTA/FASTQ format (can be compressed with gzip)\n"
       "\n"
       "  options:\n"
+      "    -f, --frequencies\n"
+      "      default: counts\n"
+      "      technique to detect variants\n"
+      "    -k, --ksw2\n"
+      "      default: edlib\n"
+      "      used alignment method\n"
       "    -t, --threads <int>\n"
       "      default: 1\n"
       "      number of threads\n"
@@ -80,12 +90,16 @@ void Help() {
 }  // namespace
 
 int main(int argc, char** argv) {
+  bool use_frequencies = false;
+  bool use_ksw2 = false;
   std::uint32_t num_threads = 1;
 
-  std::string optstr = "t:h";
+  std::string optstr = "fkt:h";
   int arg;
   while ((arg = getopt_long(argc, argv, optstr.c_str(), options, nullptr)) != -1) {  // NOLINT
     switch (arg) {
+      case 'f': use_frequencies = true; break;
+      case 'k': use_ksw2 = true; break;
       case 't': num_threads = atoi(optarg); break;
       case 'v': std::cout << VERSION << std::endl; return 0;
       case 'h': Help(); return 0;
@@ -133,6 +147,144 @@ int main(int argc, char** argv) {
   auto thread_pool = std::make_shared<thread_pool::ThreadPool>(num_threads);
   ram::MinimizerEngine minimizer_engine{thread_pool, 15U, 5U};
 
+  auto edlib_wrapper = [&] (
+      std::uint32_t i,
+      const biosoup::Overlap& it,
+      const std::string& lhs,
+      const std::string& rhs) -> void {
+    EdlibAlignResult result = edlibAlign(
+        lhs.c_str(), lhs.size(),
+        rhs.c_str(), rhs.size(),
+        edlibNewAlignConfig(-1, EDLIB_MODE_NW, EDLIB_TASK_PATH, nullptr, 0));
+    if (result.status == EDLIB_STATUS_OK) {
+      std::uint32_t lhs_pos = it.lhs_begin;
+      std::uint32_t rhs_pos = 0;
+      for (int j = 0; j < result.alignmentLength; ++j) {
+        switch (result.alignment[j]) {
+          case 0:
+          case 3: {
+            switch (rhs[rhs_pos]) {
+              case 'A': ++piles[i][lhs_pos].a; break;
+              case 'C': ++piles[i][lhs_pos].c; break;
+              case 'G': ++piles[i][lhs_pos].g; break;
+              case 'T': ++piles[i][lhs_pos].t; break;
+              default: break;
+            }
+            ++lhs_pos;
+            ++rhs_pos;
+            break;
+          }
+          case 1: {
+            ++piles[i][lhs_pos].i;
+            ++lhs_pos;
+            break;
+          }
+          case 2: {
+            ++rhs_pos;
+            break;
+          }
+          default: break;
+        }
+      }
+    }
+    edlibFreeAlignResult(result);
+  };
+
+  auto ksw2_wrapper = [&] (
+      std::uint32_t i,
+      const biosoup::Overlap& it,
+      const std::string& lhs,
+      const std::string& rhs) -> void {
+    std::int8_t m = 3;
+    std::int8_t n = -5;
+    std::int8_t g = 4;
+    std::int8_t e = 4;
+    std::int8_t mn[25] = {
+        m, n, n, n, 0,
+        n, m, n, n, 0,
+        n, n, m, n, 0,
+        n, n, n, m, 0,
+        0, 0, 0, 0, 0
+    };
+
+    std::unordered_map<char, std::uint8_t> transform = {
+      {'A', 0}, {'a', 0},
+      {'C', 1}, {'c', 1},
+      {'G', 2}, {'g', 2},
+      {'T', 3}, {'t', 3}
+    };
+
+    auto lhs_ = new std::uint8_t[lhs.size()];
+    for (std::size_t j = 0; j < lhs.size(); ++j) {
+      lhs_[j] = transform[lhs[j]];
+    }
+
+    auto rhs_ = new std::uint8_t[rhs.size()];
+    for (std::size_t j = 0; j < rhs.size(); ++j) {
+      rhs_[j] = transform[rhs[j]];
+    }
+
+    int m_cigar = 0, n_cigar = 0;
+    std::uint32_t* cigar = nullptr;
+
+    auto score = ksw_gg2_sse(
+        nullptr,   // void *km
+        lhs.size(),  // int qlen
+        lhs_,  // const uint8_t *query
+        rhs.size(),  // int tlen
+        rhs_,  // const uint8_t *target
+        5,  // int8_t m
+        mn,  // const int8_t *mat
+        g,  // int8_t gapo
+        e,  // int8_t gape
+        -1,  // int w
+        &m_cigar,  // int *m_cigar_
+        &n_cigar,  // int *n_cigar_
+        &cigar);  // uint32_t **cigar_
+
+    if (score > 0 && n_cigar > 0) {
+      std::uint32_t lhs_pos = it.lhs_begin;
+      std::uint32_t rhs_pos = 0;
+      for (std::size_t j = 0; j < static_cast<std::size_t>(n_cigar); ++j) {
+        std::size_t count = cigar[j] >> 4;
+        std::size_t op = cigar[j] & 15;
+        switch (op) {
+          case 0: {  // M
+            for (std::size_t k = 0; k < count; ++k) {
+              switch (rhs[rhs_pos + k]) {
+                case 'A': ++piles[i][lhs_pos + k].a; break;
+                case 'C': ++piles[i][lhs_pos + k].c; break;
+                case 'G': ++piles[i][lhs_pos + k].g; break;
+                case 'T': ++piles[i][lhs_pos + k].t; break;
+                default: break;
+              }
+            }
+            lhs_pos += count;
+            rhs_pos += count;
+            break;
+          }
+          case 1: {  // I
+            for (std::size_t k = 0; k < count; ++k) {
+              ++piles[i][lhs_pos + k].i;
+            }
+            lhs_pos += count;
+            break;
+          }
+          case 2: {  // D
+            rhs_pos += count;
+            break;
+          }
+          default: break;
+        }
+      }
+    }
+
+    free(cigar);
+
+    delete[] rhs_;
+    delete[] lhs_;
+  };
+
   std::size_t bytes = 0;
   for (std::uint32_t i = 0, j = 0; i < sequences.size(); ++i) {
     bytes += sequences[i]->inflated_len;
@@ -174,42 +326,11 @@ int main(int argc, char** argv) {
               }
               auto rhs = rhs_.InflateData();
 
-              EdlibAlignResult result = edlibAlign(
-                  lhs.c_str(), lhs.size(),
-                  rhs.c_str(), rhs.size(),
-                  edlibNewAlignConfig(-1, EDLIB_MODE_NW, EDLIB_TASK_PATH, nullptr, 0));  // NOLINT
-              if (result.status == EDLIB_STATUS_OK) {
-                std::uint32_t lhs_pos = it.lhs_begin;
-                std::uint32_t rhs_pos = 0;
-                for (int j = 0; j < result.alignmentLength; ++j) {
-                  switch (result.alignment[j]) {
-                    case 0:
-                    case 3: {
-                      switch (rhs[rhs_pos]) {
-                        case 'A': ++piles[i][lhs_pos].a; break;
-                        case 'C': ++piles[i][lhs_pos].c; break;
-                        case 'G': ++piles[i][lhs_pos].g; break;
-                        case 'T': ++piles[i][lhs_pos].t; break;
-                        default: break;
-                      }
-                      ++lhs_pos;
-                      ++rhs_pos;
-                      break;
-                    }
-                    case 1: {
-                      ++piles[i][lhs_pos].i;
-                      ++lhs_pos;
-                      break;
-                    }
-                    case 2: {
-                      ++rhs_pos;
-                      break;
-                    }
-                    default: break;
-                  }
-                }
+              if (use_ksw2) {
+                ksw2_wrapper(i, it, lhs, rhs);
+              } else {
+                edlib_wrapper(i, it, lhs, rhs);
               }
-              edlibFreeAlignResult(result);
             }
           },
           k));
@@ -244,15 +365,26 @@ int main(int argc, char** argv) {
     std::size_t j = 0;
     for (const auto& jt : it) {
       std::vector<std::uint32_t> counts = { jt.a, jt.c, jt.g, jt.t };
-      double sum = std::accumulate(counts.begin(), counts.end(), jt.i);
+      double sum = std::accumulate(counts.begin(), counts.end(), 0);
+      if (use_frequencies) {
+        for (auto& kt : counts) {
+          kt /= sum;
+        }
+      }
       if (sum == 0.) {
         ++zeros;
       } else if (sum > 7.) {
         ++predictable;
         std::size_t variants = 0;
         for (const auto& it : counts) {
-          if (it > 3) {
-            ++variants;
+          if (use_frequencies) {
+            if (0.333 < it && it < 0.666) {
+              ++variants;
+            }
+          } else {
+            if (it > 3) {
+              ++variants;
+            }
           }
         }
         if (variants > 1) {
